c0028318eb48b0311a71ee29c82a794f1a625301
youssefmohamed93 <142684130+youssefmohamed93@users.noreply.github.com>
2024-01-19 04:34:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Navbar Menu Icon //
+let menu = document.querySelector('#menu-box');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('menu-box')
+    navbar.classList.toggle('open')
+}
+
+// Scroll Reveal //
+const sr = ScrollReveal (
+    {
+        distance: '40px',
+        duration: 2050,
+        delay: 200,
+        reset: true
+    }
+);
+
+sr.reveal('.home-text',{origin: 'top'});
+sr.reveal('.home-image img',{origin: 'top'});
+sr.reveal('.about-image',{origin: 'top'});
+sr.reveal('.about-text',{origin: 'top'});
+sr.reveal('.services-box',{origin: 'top'});
